15c57b9af48fd08cf827748ea1d6c0653f64ac30
Andres <andesc2007@gmail.com>
2026-03-02 14:17:52 -0500

    set up full stack integration

---
+++ Backend/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import pandas as pd
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import pandas as pd
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return "Backend is running"
+
+@app.route("/calculate", methods=["POST"])
+def calculate():
+    data = request.json
+    product = data["product"]
+
+    # Fake test calculation for now
+    carbon_value = len(product) * 2
+
+    return jsonify({
+        "product": product,
+        "carbon": carbon_value
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ README.md
@@ -0,0 +1,7 @@
+The first four things that should be done when booting up VSCode:
+
+1. PULL FROM THE GIT REPOSITORY. ALWAYS
+Then, in terminal, run the following commands:
+2. venv/Scripts/activate        (if the virtual environent isn't already running)
+3. cd Backend                   (if you're not already in Backend)
+4. python app.py
