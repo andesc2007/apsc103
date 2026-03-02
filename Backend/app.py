a118903dde00122711586252ab4cd6961e735a31
Andres <andesc2007@gmail.com>
2026-03-02 14:26:25 -0500

    small change to app.py

--- Backend/app.py
+++ Backend/app.py
@@ -2,10 +2,6 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pandas as pd
 
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-import pandas as pd
-
 app = Flask(__name__)
 CORS(app)
 
