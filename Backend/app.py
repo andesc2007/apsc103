84c785f3a63de9831995623a622e2c29e5cbc440
Andres <andesc2007@gmail.com>
2026-03-13 02:31:17 -0400

    small changes to app.py

--- Backend/app.py
+++ Backend/app.py
@@ -1,23 +1,21 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import pandas as pd
 
-app = Flask(__name__)   # Creates the actual application
-CORS(app)               # Ensures that the app can run in a web browser
+app = Flask(__name__)
+CORS(app)
 
-@app.route("/") # Once someone visits the home page, this function will run
+@app.route("/")
 def home():
     return "Backend is running"
 
-@app.route("/calculate", methods=["POST"]) #When /calculate is fetched, this function will run
+@app.route("/calculate", methods=["POST"])
 def calculate():
     data = request.json
-    product = data["product"] # Receives product pushed by JS function
+    product = data["product"]
 
-    # Fake test calculation for now. Will be updated to support logic.py and csv files, MAYBE an API?
-    carbon_value = len(product) * 2
+    carbon_value = len(product)
 
-    return jsonify({ # Returns the carbon value to the frontend
+    return jsonify({
         "product": product,
         "carbon": carbon_value
     })
